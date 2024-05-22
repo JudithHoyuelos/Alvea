32217db5823ebe027690ca7a596caa7f3d0deb13
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-05-22 09:53:37 +0200

    eliminacion del punto de luz

--- assets/js/Luzambiental.js
+++ assets/js/Luzambiental.js
@@ -6,8 +6,5 @@ const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
 directionalLight.position.set(-10,10,-10);
 lights.add(directionalLight);
 
-const pointLight = new THREE.PointLight( 0xff0000, 3, 100 );
-pointLight.position.set( 0, 3, 5 );
-lights.add(pointLight)
 
 export default lights
--- assets/js/Puntoluz.js
+++
@@ -1,6 +0,0 @@
-import THREE from './three';
-
-const light = new THREE.PointLight( 0xff0000, 1, 100 );
-light.position.set( 2, 2, 2 );
-
-export default light
