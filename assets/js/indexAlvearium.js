5a0c6c6cdc5444589f1c7523bdee20341d28e374
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-05-27 14:35:15 +0200

    hotpoints y exit con el menu lateral

--- assets/js/indexAlvearium.js
+++ assets/js/indexAlvearium.js
@@ -45,6 +45,22 @@ window.addEventListener('resize', () => {
 // controls.target.set(2, 1, 0);
 // controls.update();
 
+const menu = document.querySelector("#menu");
+menu.addEventListener('click', onClickMenu);
+const exit = document.querySelector("#exit");
+exit.addEventListener('click', onClickExit);
+
+function onClickMenu(e) {
+    const div = document.querySelector("#desplegable");
+    div.style.display = '';
+    menu.style.display = 'none';
+}
+
+function onClickExit(e) {
+    const div = document.querySelector("#desplegable");
+    div.style.display = 'none';
+    menu.style.display = '';
+}
 
 // PRUEBA CARGAR MODELO 3D
 let modelo = new GLTFLoader();
@@ -78,131 +94,22 @@ modelo.load(
     }
 );
 
-// function updateAnnotationPosition() {
-//     const vector = new THREE.Vector3(-0.1, -3, 3);
-//     vector.project(camera);
-//     vector.x = Math.round((0.5 + vector.x / 2) * window.innerWidth);
-//     vector.y = Math.round((0.5 - vector.y / 2) * window.innerHeight);
-//     const vector2 = new THREE.Vector3(-0.1, 3, 3);
-//     vector2.project(camera);
-//     vector2.x = Math.round((0.5 + vector2.x / 2) * window.innerWidth);
-//     vector2.y = Math.round((0.5 - vector2.y / 2) * window.innerHeight);
-//     const vector3 = new THREE.Vector3(10, 0, 3);
-//     vector3.project(camera);
-//     vector3.x = Math.round((0.5 + vector3.x / 2) * window.innerWidth);
-//     vector3.y = Math.round((0.5 - vector3.y / 2) * window.innerHeight);
-//     const vector4 = new THREE.Vector3(-12, 0, 3);
-//     vector4.project(camera);
-//     vector4.x = Math.round((0.5 + vector4.x / 2) * window.innerWidth);
-//     vector4.y = Math.round((0.5 - vector4.y / 2) * window.innerHeight);
-//     const annotation = document.querySelector('#punto1');
-//     annotation.style.top = `${vector.y}px`;
-//     annotation.style.left = `${vector.x}px`;
-//     annotation.addEventListener("mouseover", (e)=> {
-//         modales.style.top = `${vector.y}px`;
-//         modales.style.left = `${vector.x}px`;
-//         annotation.style.display='';
-//     })
-//     const annotation2 = document.querySelector('#punto2');
-//     annotation2.style.top = `${vector2.y}px`;
-//     annotation2.style.left = `${vector2.x}px`;
-//     const annotation3 = document.querySelector('#punto3');
-//     annotation3.style.top = `${vector3.y}px`;
-//     annotation3.style.left = `${vector3.x}px`;
-//     const annotation4 = document.querySelector('#punto4');
-//     annotation4.style.top = `${vector4.y}px`;
-//     annotation4.style.left = `${vector4.x}px`;
-// }
-
-// Función para mover la cámara a una posición específica
-// function onClick(x, y, z, lookAtX, lookAtY, lookAtZ, puntos) {
-//     // Ocultar los puntos
-//     puntos.forEach(punto => punto.style.display = 'none');
-//     return function(event) {
-//         gsap.to(camera.position, {
-//             duration: 2,
-//             x: x,
-//             y: y,
-//             z: z,
-//             onUpdate: function () {
-//                 camera.lookAt(lookAtX, lookAtY, lookAtZ);
-//             }
-//         });
-//     }
-// }
-
-// function updateAnnotationPosition() {
-//     const modales = document.querySelector('#modales');
-
-//     // Helper function to update position
-//     function updatePosition(vector, selector) {
-//         vector.project(camera);
-//         vector.x = Math.round((0.5 + vector.x / 2) * window.innerWidth);
-//         vector.y = Math.round((0.5 - vector.y / 2) * window.innerHeight);
-//         const annotation = document.querySelector(selector);
-//         if (annotation) {
-//             annotation.style.top = `${vector.y}px`;
-//             annotation.style.left = `${vector.x}px`;
-//         }
-//         return { x: vector.x, y: vector.y, element: annotation };
-//     }
-
-//     // Add event listeners to an annotation
-//     function addAnnotationEvents(position, offsetX, offsetY, cameraX, cameraY, cameraZ, lookAtX, lookAtY, lookAtZ, puntos) {
-//         if (position.element) {
-//             position.element.addEventListener('click', onClick(cameraX, cameraY, cameraZ, lookAtX, lookAtY, lookAtZ, puntos));
-//             position.element.addEventListener("mouseover", (e) => {
-//                 if (modales) {
-//                     modales.style.display = '';
-//                     modales.style.top = `${position.y + offsetY}px`;
-//                     modales.style.left = `${position.x + offsetX}px`;
-//                     console.log('aparece');
-//                 } else {
-//                     console.log('no existe el modal');
-//                 }
-//             });
-//             position.element.addEventListener("mouseout", (e) => {
-//                 if (modales) {
-//                     modales.style.display = 'none';
-//                     console.log('desaparece');
-//                 } else {
-//                     console.log('no existe el modal');
-//                 }
-//             });
-//         } else {
-//             console.log('no existe');
-//         }
-//     }
-
-//     const position1 = updatePosition(new THREE.Vector3(2, -3, 3), '#punto1');
-//     const position2 = updatePosition(new THREE.Vector3(2, 3, 3), '#punto2');
-//     const position3 = updatePosition(new THREE.Vector3(12, 0, 3), '#punto3');
-//     const position4 = updatePosition(new THREE.Vector3(-8, 0, 3), '#punto4');
-
-//     // Gather all point elements
-//     const puntos = [position1.element, position2.element, position3.element, position4.element];
-
-//     // Add events to each annotation with their respective camera positions and lookAt positions
-//     addAnnotationEvents(position1, 39, -35, 2.3888577103551727, 1.8324140151651975, 2.704188278006996, -1, 1, 0, puntos);
-//     addAnnotationEvents(position2, 39, -35, 2.1163238507698527, 1.8324140151651975, 3.349330773199133, 2, 1, 6, puntos); // Actualiza estos valores según sea necesario
-//     addAnnotationEvents(position3, 39, -35, 7.238170413905775, 1.8324140151651975, -4.753328529998346, 10, 1, 0, puntos); // Actualiza estos valores según sea necesario
-//     addAnnotationEvents(position4, 39, -35, -2.1208780253940085, 1.8324140151651953, -0.04582836480489122, -10, 1, 0, puntos); // Actualiza estos valores según sea necesario
-// }
-
-// Llama a la función para asegurarte de que los elementos DOM existen antes de actualizar
-document.addEventListener('DOMContentLoaded', (event) => {
-    updateAnnotationPosition();
-});
-
-// Función para mover la cámara a una posición específica y ocultar los puntos
-function onClick(x, y, z, lookAtX, lookAtY, lookAtZ, points) {
+// Función para mover la cámara al inicio y que aparezcan los puntos del inicio
+function onClickLogo(x, y, z, lookAtX, lookAtY, lookAtZ, oldPoints, newPoints) {
     return function(event) {
-        // Ocultar los puntos antes de mover la cámara
-        points.forEach(point => {
-            if (point.element) {
-                point.element.style.display = 'none';
-            }
-        });
+        const arriba = document.querySelector("#useCase");
+        if (newPoints == arriba) {
+            arriba.style.display = 'none';
+        } else {
+            // Ocultar los puntos antiguos antes de mover la cámara
+            newPoints.forEach(point => {
+                if (point.element) {
+                    point.element.style.display = 'none';
+                }
+            });
+        }
+
+        const logo = document.querySelector('#inicio');
 
         gsap.to(camera.position, {
             duration: 2,
@@ -212,14 +119,63 @@ function onClick(x, y, z, lookAtX, lookAtY, lookAtZ, points) {
             onUpdate: function () {
                 camera.lookAt(lookAtX, lookAtY, lookAtZ);
             },
-            // onComplete: function() {
-            //     // Mostrar los puntos después de que la cámara haya terminado de moverse (opcional)
-            //     points.forEach(point => {
-            //         if (point.element) {
-            //             point.element.style.display = 'block';
-            //         }
-            //     });
-            // }
+            onComplete: function() {
+                // Mostrar los nuevos puntos después de que la cámara haya terminado de moverse
+                oldPoints.forEach(point => {
+                    if (point.element) {
+                        point.element.style.display = '';
+                    }
+                    if (logo) {
+                        logo.style.display = 'none';
+                    }
+                });
+            }
+        });
+    }
+}
+
+
+// Función para mover la cámara a una posición específica y ocultar los puntos
+function onClick(x, y, z, lookAtX, lookAtY, lookAtZ, oldPoints, newPoints) {
+    return function(event) {
+        // Ocultar los puntos antiguos antes de mover la cámara
+        oldPoints.forEach(point => {
+            if (point.element) {
+                point.element.style.display = 'none';
+            }
+        });
+
+        const logo = document.querySelector('#inicio');
+        const arriba = document.querySelector("#useCase");
+
+        gsap.to(camera.position, {
+            duration: 2,
+            x: x,
+            y: y,
+            z: z,
+            onUpdate: function () {
+                camera.lookAt(lookAtX, lookAtY, lookAtZ);
+            },
+            onComplete: function() {
+                // Mostrar los nuevos puntos después de que la cámara haya terminado de moverse
+                if (newPoints == arriba) {
+                    arriba.style.display = '';
+                    if (logo) {
+                        logo.style.display = '';
+                        logo.addEventListener('click', onClickLogo(1.5620123612353716, 1.8324140151651975, -12.611046376873789, 2, 1, 0, oldPoints, newPoints))
+                    }
+                } else {
+                    newPoints.forEach(point => {
+                        if (point.element) {
+                            point.element.style.display = '';
+                        }
+                        if (logo) {
+                            logo.style.display = '';
+                            logo.addEventListener('click', onClickLogo(1.5620123612353716, 1.8324140151651975, -12.611046376873789, 2, 1, 0, oldPoints, newPoints))
+                        }
+                    });
+                }
+            }
         });
     }
 }
@@ -241,9 +197,9 @@ function updateAnnotationPosition() {
     }
 
     // Add event listeners to an annotation
-    function addAnnotationEvents(position, offsetX, offsetY, cameraX, cameraY, cameraZ, lookAtX, lookAtY, lookAtZ, points) {
+    function addAnnotationEvents(position, offsetX, offsetY, cameraX, cameraY, cameraZ, lookAtX, lookAtY, lookAtZ, oldPoints, newPoints) {
         if (position.element) {
-            position.element.addEventListener('click', onClick(cameraX, cameraY, cameraZ, lookAtX, lookAtY, lookAtZ, points));
+            position.element.addEventListener('click', onClick(cameraX, cameraY, cameraZ, lookAtX, lookAtY, lookAtZ, oldPoints, newPoints));
             position.element.addEventListener("mouseover", (e) => {
                 if (modales) {
                     modales.style.display = '';
@@ -272,14 +228,30 @@ function updateAnnotationPosition() {
     const position3 = updatePosition(new THREE.Vector3(12, 0, 3), '#punto3');
     const position4 = updatePosition(new THREE.Vector3(-8, 0, 3), '#punto4');
 
+    const position5 = updatePosition(new THREE.Vector3(3, 4, 3), '#punto5');
+    const position6 = updatePosition(new THREE.Vector3(8, 2, 3), '#punto6');
+    const position7 = updatePosition(new THREE.Vector3(0, 0, 3), '#punto7');
+
+    const position8 = updatePosition(new THREE.Vector3(13, 4, 3), '#punto8');
+    const position9 = updatePosition(new THREE.Vector3(2, 5, 3), '#punto9');
+    const position10 = updatePosition(new THREE.Vector3(1, -1.5, 3), '#punto10');
+
+    const position11 = updatePosition(new THREE.Vector3(-4, -1.5, 3), '#punto11');
+    const position12 = updatePosition(new THREE.Vector3(-7, -1.5, 3), '#punto12');
+    const position13 = updatePosition(new THREE.Vector3(-15, -6, 3), '#punto13');
+
     // Array de posiciones para pasarlas a la función onClick
-    const positions = [position1, position2, position3, position4];
+    const oldPositions = [position1, position2, position3, position4];
+    const newPositionsDerecha = [position5, position6, position7];
+    const newPositionsIzquierda = [position8, position9, position10];
+    const newPositionsAbajo = [position11, position12, position13];
+    const arriba = document.querySelector("#useCase");
 
     // Add events to each annotation with their respective camera positions and lookAt positions
-    addAnnotationEvents(position1, 39, -35, 2.3888577103551727, 1.8324140151651975, 2.704188278006996, -1, 1, 0, positions);
-    addAnnotationEvents(position2, 39, -35, 2.1163238507698527, 1.8324140151651975, 3.349330773199133, 2, 1, 6, positions); // Actualiza estos valores según sea necesario
-    addAnnotationEvents(position3, 39, -35, 7.238170413905775, 1.8324140151651975, -4.753328529998346, 10, 1, 0, positions); // Actualiza estos valores según sea necesario
-    addAnnotationEvents(position4, 39, -35, -2.1208780253940085, 1.8324140151651953, -0.04582836480489122, -10, 1, 0, positions); // Actualiza estos valores según sea necesario
+    addAnnotationEvents(position1, 39, -35, 2.3888577103551727, 1.8324140151651975, 2.704188278006996, -1, 1, 0, oldPositions, newPositionsAbajo);
+    addAnnotationEvents(position2, 39, -35, 2.1163238507698527, 1.8324140151651975, 3.349330773199133, 2, 1, 6, oldPositions, arriba); // Actualiza estos valores según sea necesario
+    addAnnotationEvents(position3, 39, -35, 7.238170413905775, 1.8324140151651975, -4.753328529998346, 10, 1, 0, oldPositions, newPositionsIzquierda); // Actualiza estos valores según sea necesario
+    addAnnotationEvents(position4, 39, -35, -2.1208780253940085, 1.8324140151651953, -0.04582836480489122, -10, 1, 0, oldPositions, newPositionsDerecha); // Actualiza estos valores según sea necesario
 }
 
 // Llama a la función para asegurarte de que los elementos DOM existen antes de actualizar
@@ -297,9 +269,9 @@ let controles = new PointerLockControls(camera, renderer.domElement);
 
 let xdir = 0, zdir = 0
 
-document.onclick = () => {
-    controles.lock();
-}
+// document.onclick = () => {
+//     controles.lock();
+// }
 
 document.addEventListener('keydown', (e) => {
     switch (e.keyCode) {
@@ -341,8 +313,6 @@ let delta, tiempoI, tiempoF, vel
 tiempoI = Date.now();
 
 vel = 10;
-
-// updateAnnotationPosition();
 
 function animate() {
 
