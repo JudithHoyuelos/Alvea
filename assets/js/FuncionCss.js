e0eae594f9c0ca9d9d926d7e5cc7c3cfa02f1c89
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-06-03 15:42:08 +0200

    errores solucionados

--- assets/js/FuncionCss.js
+++ assets/js/FuncionCss.js
@@ -8,7 +8,7 @@ const flechaDerecha = document.getElementById('flecha-derecha');
 
 flechaDerecha.addEventListener('click', () => {
     fila.scrollLeft += fila.offsetWidth;
-    const indicadorActivo = document.querySelector('.indicadores .activo');
+    const indicadorActivo = document.querySelector('.roadmap-rayas .activo');
     if (indicadorActivo.nextSibling) {
         indicadorActivo.nextSibling.classList.add('activo');
         indicadorActivo.classList.remove('activo');
@@ -17,7 +17,7 @@ flechaDerecha.addEventListener('click', () => {
 
 flechaIzquierda.addEventListener('click', () => {
     fila.scrollLeft -= fila.offsetWidth;
-    const indicadorActivo = document.querySelector('.indicadores .activo');
+    const indicadorActivo = document.querySelector('.roadmap-rayas .activo');
     if (indicadorActivo.previousSibling) {
         indicadorActivo.previousSibling.classList.add('activo');
         indicadorActivo.classList.remove('activo');
@@ -33,10 +33,10 @@ for (let i = 0; i < numeroPaginas; i++) {
     if (i === 0) {
         indicador.classList.add('activo');
     }
-    document.querySelector('.indicadores').appendChild(indicador);
+    document.querySelector('.roadmap-rayas').appendChild(indicador);
     indicador.addEventListener('click', (e) => {
         fila.scrollLeft = i * fila.offsetWidth;
-        document.querySelector('.indicadores .activo').classList.remove('activo');
+        document.querySelector('.roadmap-rayas .activo').classList.remove('activo');
         e.target.classList.add('activo');
     })
 }
