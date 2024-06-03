3521364f8956c3bb705d8af543906ca1b721c0e5
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-06-03 12:55:52 +0200

    medio funcionamiento

--- assets/js/FuncionCss.js
+++ assets/js/FuncionCss.js
@@ -1,14 +1,18 @@
 // ----------- movimiento del carrusel y flechas
 
-const fila = document.querySelector('.contenedor-carrusel');
-const peliculas = document.querySelectorAll('.pelicula');
+const fila = document.querySelector('.roadmap-cards');
+console.log(fila)
+const tarjetas = document.querySelectorAll('.roadmap-card');
+console.log(tarjetas)
 
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
 
 flechaDerecha.addEventListener('click', () => {
+    console.log("le he dado a la flecha derecha")
     fila.scrollLeft += fila.offsetWidth;
-    const indicadorActivo = document.querySelector('.indicadores .activo');
+    const indicadorActivo = document.querySelector('.roadmap-rayas .activo');
+    console.log(indicadorActivo)
     if (indicadorActivo.nextSibling) {
         indicadorActivo.nextSibling.classList.add('activo');
         indicadorActivo.classList.remove('activo');
@@ -16,8 +20,9 @@ flechaDerecha.addEventListener('click', () => {
 });
 
 flechaIzquierda.addEventListener('click', () => {
+    console.log("le he dado a la flecha izquierda")
     fila.scrollLeft -= fila.offsetWidth;
-    const indicadorActivo = document.querySelector('.indicadores .activo');
+    const indicadorActivo = document.querySelector('.roadmap-rayas .activo');
     if (indicadorActivo.previousSibling) {
         indicadorActivo.previousSibling.classList.add('activo');
         indicadorActivo.classList.remove('activo');
@@ -27,16 +32,16 @@ flechaIzquierda.addEventListener('click', () => {
 
 // ----------------- indicador y contador de paginas
 
-const numeroPaginas = Math.ceil(peliculas.length / 3);
+const numeroPaginas = Math.ceil(tarjetas.length / 1);
 for (let i = 0; i < numeroPaginas; i++) {
     const indicador = document.createElement('button');
     if (i === 0) {
         indicador.classList.add('activo');
     }
-    document.querySelector('.indicadores').appendChild(indicador);
+    document.querySelector('.roadmap-rayas').appendChild(indicador);
     indicador.addEventListener('click', (e) => {
         fila.scrollLeft = i * fila.offsetWidth;
-        document.querySelector('.indicadores .activo').classList.remove('activo');
+        document.querySelector('.roadmap-rayas .activo').classList.remove('activo');
         e.target.classList.add('activo');
     })
 }
