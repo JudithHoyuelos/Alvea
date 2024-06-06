a4baa96b290ca72f3a97d7b93d4004f9f4a22ff9
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-06-06 09:51:01 +0200

    funcionalidad de equipo y etiquetas listas

--- assets/js/FuncionCss.js
+++ assets/js/FuncionCss.js
@@ -45,7 +45,7 @@ flechaDerecha.addEventListener('click', () => {
         // indicadorActivo = document.querySelector('.roadmap-rayas .botones-rayas');
         // console.log(indicadorActivo)
         indicadorActivo.classList.remove('activo');
-        indicadorActivo.previousElementSibling.classList.add('activo');
+        // indicadorActivo.previousElementSibling.classList.add('activo');
         // indicadorActivo.previousElementSibling.classList.add('activo');
     } else {
         console.log('paso al else')
@@ -86,5 +86,22 @@ for (let i = 0; i < numeroPaginas; i++) {
     })
 }
 
+// slider
+
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.mySwiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            depth: 500,
+            modifier: 1,
+            slideShadows: true,
+            rotate: 0,
+            stretch: 0
+        }
+    });
+});
 
 
