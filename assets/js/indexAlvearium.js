3a7092e424b0379abb74f0d43af1e44a7114c9dd
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-07-02 12:03:23 +0200

    prueba para pages v14

--- assets/js/indexAlvearium.js
+++ assets/js/indexAlvearium.js
@@ -1,4 +1,5 @@
-import THREE from './three.js';
+// import THREE from './three.js';
+import THREE from './path/to/three.js';
 // import { gsap } from '@vendor/gsap/gsap.min.js';
 import { OrbitControls } from '../vendor/three/examples/jsm/controls/OrbitControls.js';
 import { PointerLockControls } from '../vendor/three/examples/jsm/controls/PointerLockControls.js';
