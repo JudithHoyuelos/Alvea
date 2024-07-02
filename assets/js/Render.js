8a1ede505b4f3fb5a0cfa029010471a4045aa48f
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-07-02 11:51:35 +0200

    prueba para pages v13

--- assets/js/Render.js
+++ assets/js/Render.js
@@ -1,4 +1,4 @@
-import THREE from './three';
+import THREE from './three.js';
 
 const renderer = new THREE.WebGLRenderer({antialia: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
--- assets/js/Screen.js
+++ assets/js/Screen.js
@@ -1,4 +1,4 @@
-import THREE from './three';
+import THREE from './three.js';
 
 const scene = new THREE.Scene();
 
